4a25d80bf42c0e25247f28a6ae0d617a2daf3a9e
Sean Theiss <theiss.sean@gmail.com>
2023-03-30 11:27:43 +0200

    fix: management of the number of arguments

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,4 +1,8 @@
 import sys
 
+if(len(sys.argv)!=2):
+    print("ERROR: wrong number of arguments. Try with one argument only after the function's call.")
+    sys.exit()
+
 for arg in sys.argv[1:]:
     print (arg)
