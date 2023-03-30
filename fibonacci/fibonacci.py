79bfed68ad23215cb1e6085cbb33eb7dd86912ce
Sean Theiss <theiss.sean@gmail.com>
2023-03-30 16:28:01 +0200

    fix: add function fibonacci (non-recursive) and call it with the argument

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -4,7 +4,26 @@ if(len(sys.argv)!=2):
     print("ERROR: wrong number of arguments. Try with one argument only after the function's call.")
     sys.exit()
 
+val = 0
+
 try:
-    print(int(sys.argv[1]))
+    val = int(sys.argv[1])
 except:
-    print("ERROR: argument is not a number.")
+    print("ERROR: argument is not a number. Try with a number.")
+    sys.exit()
+
+if(val<1):
+    print("ERROR: argument lower than 1. Try with a bigger value.")
+
+# First value of the Fibonacci's sequence is 0 and the second is 1. The element Fn+2 = Fn+1 + Fn
+def fibonacci(count):
+    stock = 0
+    nb1 = 0
+    nb2 = 1
+    for i in range(count):
+        stock = nb1 + nb2
+        nb1 = nb2
+        nb2 = stock
+    return nb1
+
+print(fibonacci(val-1))
