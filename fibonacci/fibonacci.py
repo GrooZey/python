19847a4a72987360dfdf09635e1feb7999e81f41
Sean Theiss <theiss.sean@gmail.com>
2023-03-30 16:02:15 +0200

    fix: management of case if the argument isn't a number in fibonacci.py

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -4,5 +4,7 @@ if(len(sys.argv)!=2):
     print("ERROR: wrong number of arguments. Try with one argument only after the function's call.")
     sys.exit()
 
-for arg in sys.argv[1:]:
-    print (arg)
+try:
+    print(int(sys.argv[1]))
+except:
+    print("ERROR: argument is not a number.")
